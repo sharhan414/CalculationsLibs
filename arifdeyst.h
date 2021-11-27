d9fd4a191b454cf9d05686d0ff70198173e6cf54
sharhan414 <ssoltygov98@mail.ru>
2021-11-27 21:32:53 +0300

    Update

--- arifdeyst.h
+++ arifdeyst.h
@@ -11,7 +11,6 @@ public:
 
     void arif_detstvie(double& otvet,std::string m);
 private:
-    void raspredDeystAndValues(std::vector<std::string>&,std::vector<char>&,std::string&);
     void  ReshArifDeystUmnjAndDeln(std::vector<double>&, std::vector<char>&);//решаеть ариф.действия умножение и деление
     double ReshArifDeystPlusAndMinus(std::vector<double>&, std::vector<char>& );
     void getArifSignsForExp(std::string &text,std::vector<char>& ArithmeticSigns);
