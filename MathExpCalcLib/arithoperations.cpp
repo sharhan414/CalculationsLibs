aefa90b290ace7b38f8987a2a5fec35c4fb57ddc
sharhan414 <ssoltygov98@mail.ru>
2022-06-27 19:57:05 +0300

    update: replacement arifdest with arithoperation 27.06.2022

---
+++ MathExpCalcLib/arithoperations.cpp
@@ -0,0 +1,6 @@
+#include "arithoperations.h"
+
+ArithOperations::ArithOperations()
+{
+
+}
---
+++ MathExpCalcLib/arithoperations.h
@@ -0,0 +1,11 @@
+#ifndef ARITHOPERATIONS_H
+#define ARITHOPERATIONS_H
+
+
+class ArithOperations
+{
+public:
+    ArithOperations();
+};
+
+#endif // ARITHOPERATIONS_H
